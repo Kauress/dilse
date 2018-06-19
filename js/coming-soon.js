3549182c0678b485917330ab3742cc35b8a55577
kauress8 <kauress8@gmail.com>
2018-06-19 12:42:17 -0700

    initial commit

---
+++ js/coming-soon.js
@@ -0,0 +1,22 @@
+
+
+$("#btn1" ).click(function() {
+  $("#birthdaypic").toggle();
+  $("#birthdaymsg").toggle();
+
+});
+
+//function addimage() {
+  //const imageurl= "url('img/lol.jpg')";
+    // document.body.style.backgroundImage = imageurl;
+  //}
+
+  $('#btn2').click(function() {
+      $('body').toggleClass('BgClass');
+  });
+
+
+  $("#btn3" ).click(function() {
+    $("#heartmsg").toggle();
+
+  });
